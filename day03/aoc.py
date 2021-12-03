e43b52e973154931c13b3632e32553b21cd003d2
Per Holmäng <per.holmang@gmail.com>
2021-12-03 12:46:45 +0100

    day03 part2

--- day03/aoc.py
+++ day03/aoc.py
@@ -1,13 +1,16 @@
 from os import environ
 
 
-def most_common_bit(input, pos):
+def most_common_bit(inp, pos):
     zeros, ones = 0, 0
-    for _, b in enumerate(input):
+    for _, b in enumerate(inp):
         if b[pos] == "1":
             ones += 1
         elif b[pos] == "0":
             zeros += 1
+
+    if zeros == ones:
+        return -1
 
     return 0 if zeros > ones else 1
 
@@ -22,8 +25,34 @@ def part1(numbers):
     return int(gamma, 2) * int(epsilon, 2)
 
 
+def keep(itm, bit, pos):
+    if itm[pos] == str(bit):
+        return True
+    return False
+
+
+def oxygen_rating(numbers):
+    l = numbers
+    for i, _ in enumerate(numbers[0]):
+        mcb = most_common_bit(l, i)
+        l = list(filter(lambda n: n[i] == (str(mcb) if mcb > -1 else "1"), l))
+        if len(l) == 1:
+            return int(l[0], 2)
+
+
+def co2_scrubber_rating(numbers):
+    l = numbers
+    for i, _ in enumerate(numbers[0]):
+        mcb = most_common_bit(l, i)
+        l = list(filter(lambda n: n[i] != (str(mcb) if mcb > -1 else "1"), l))
+        if len(l) == 1:
+            return int(l[0], 2)
+
+
 def part2(numbers):
-    return 0
+    o = oxygen_rating(numbers)
+    co2 = co2_scrubber_rating(numbers)
+    return o * co2
 
 
 with open("input.txt") as f:
