d9f79335161067969fd99838cf11c890d6b920bb
Per Holmäng <per.holmang@gmail.com>
2021-12-07 09:36:49 +0100

    day07: slightly better

--- day07/aoc.py
+++ day07/aoc.py
@@ -1,24 +1,33 @@
 from os import environ
 from typing import List
 
+import numpy as np
+
 
 def tri(n):
     return int(n * (n + 1) / 2)
 
 
-def alignment_cost(crab: List[int], goal: int, triangle: bool) -> int:
+def alignment_cost(crab: List[int], goal: int, triangle: bool, max: int) -> int:
     cost = 0
+
     for c in crab:
         cost += tri(abs(c - goal)) if triangle else abs(c - goal)
+        if cost > max:
+            return -1
+
     return cost
 
 
 def cheapest_cost(crab: List[int], triangle: bool) -> int:
-    m = {}
-    for c in range(min(crab), max(crab)):
-        m[c] = alignment_cost(crab, c, triangle) if not c in m else m[c]
+    min_cost = np.Inf
+    lower, upper = min(crab), max(crab)
 
-    return min(m.values())
+    for pos in range(lower, upper):
+        fuel_cost = alignment_cost(crab, pos, triangle, min_cost)
+        min_cost = min(fuel_cost, min_cost) if fuel_cost > -1 else min_cost
+
+    return min_cost
 
 
 def part1(rows):
