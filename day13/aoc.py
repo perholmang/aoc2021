4dc19af9c0d22a73f69ad18be959e0b7baba2955
Per Holmäng <per.holmang@gmail.com>
2021-12-13 15:29:10 +0100

    day13 less lines

--- day13/aoc.py
+++ day13/aoc.py
@@ -1,7 +1,5 @@
-from os import environ
-from math import floor
 import re
-import time
+from os import environ
 
 
 def dots_to_string(dots, rows, cols):
@@ -26,31 +24,29 @@ def fold(dots, axis, pos):
     return new_dots
 
 
-def build_paper(lines):
-    rows, cols = 0, 0
+def parse_input(lines):
     dots = []
     fold_instructions = []
     for line in lines:
         if re.match("(\\d+),(\\d+)", line):
             x, y = [int(n) for n in line.split(",")]
-            rows = max(rows, y + 1)
-            cols = max(cols, x + 1)
             dots.append((x, y))
         elif re.match("fold along (x|y)=(\\d+)", line):
             match = re.match("fold along (x|y)=(\\d+)", line)
             fold_instructions.append((match[1], int(match[2])))
 
-    return dots, fold_instructions, rows, cols
+    return dots, fold_instructions
 
 
 def part1(lines):
-    dots, fold_instructions, rows, cols = build_paper(lines)
+    dots, fold_instructions = parse_input(lines)
     folded = fold(dots, fold_instructions[0][0], fold_instructions[0][1])
     return len([d for d in folded if d])
 
 
 def part2(lines):
-    dots, fold_instructions, rows, cols = build_paper(lines)
+    dots, fold_instructions = parse_input(lines)
+    rows, cols = 0, 0
     for instruction in fold_instructions:
         dots = fold(dots, instruction[0], instruction[1])
         if instruction[0] == "x":
