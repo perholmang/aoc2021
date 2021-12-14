3f38326b717ec313ab1b565c3f65297254cd0bee
Per Holmäng <per.holmang@gmail.com>
2021-12-14 12:02:31 +0100

    day14

---
+++ day14/aoc.py
@@ -0,0 +1,73 @@
+from os import environ
+
+
+def process_step(pairs_count, pairs):
+    new_count = {}
+    for _, (k, v) in enumerate(pairs_count.items()):
+        new_count[k] = v
+
+    for pair in pairs.keys():
+        count = pairs_count[pair]
+        insert_char = pairs[pair]
+        n1 = pair[0] + insert_char
+        n2 = insert_char + pair[1]
+        new_count[n1] = new_count[n1] + count
+        new_count[n2] = new_count[n2] + count
+        new_count[pair] -= count
+
+    return new_count
+
+
+def process(template, pairs, n):
+    pairs_count = {}
+
+    for pair in pairs.keys():
+        pairs_count[pair] = 0
+
+    for i in range(0, len(template) - 1):
+        pair = template[i] + template[i + 1]
+        pairs_count[pair] = 1
+
+    for i in range(0, n):
+        pairs_count = process_step(pairs_count, pairs)
+
+    char_counts = {}
+    for _, (k, v) in enumerate(pairs_count.items()):
+        if k[1] in char_counts:
+            char_counts[k[1]] += v
+        else:
+            char_counts[k[1]] = v
+
+    counts = sorted(char_counts.values())
+    return counts[-1] - counts[0]
+
+
+def part1(lines):
+    template = lines[0]
+    pairs = {}
+    for line in lines[2:]:
+        a, b = line.split(" -> ")
+        pairs[a] = b
+
+    return process(template, pairs, 10)
+
+
+def part2(lines):
+    template = lines[0]
+    pairs = {}
+    for line in lines[2:]:
+        a, b = line.split(" -> ")
+        pairs[a] = b
+
+    return process(template, pairs, 40)
+
+
+with open("input.txt") as f:
+    file_input = [line.rstrip("\n") for line in f.readlines()]
+
+print("Python")
+part = environ.get("part")
+if part == "part2":
+    print(part2(file_input))
+else:
+    print(part1(file_input))
